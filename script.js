f5bf560b1f21ff09ac9572015a924a6698413b6c
Alexander Novozhenin <anovozhenin@shatura.pro>
2022-03-24 19:36:08 +0300

    lesson 3

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 'use strict';
 
-let mainslideMenu = document.querySelector('.mainslideMenuBlack');
-let mainslideMenuList = document.querySelector('.mainslideMenuList');
+let mainslideMenu = document.querySelector('.mainslideMenu');
 let menubars = document.querySelector('.menubars');
 let krestix = document.querySelector('.krestix');
 
 function toggleMenu() {
     mainslideMenu.classList.toggle('hidden');
-    mainslideMenuList.classList.toggle('hidden');
 }
 
 menubars.addEventListener('click', toggleMenu);
